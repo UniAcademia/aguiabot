9023dbd58eb6a9cab72f1e7661fcface2dfd852f
Natan Fernandes <natanfernandes@protonmail.com>
2021-05-29 00:31:34 -0300

    Adicionado cogs e slash commands

--- aguiabot.py
+++ aguiabot.py
@@ -1,59 +1,33 @@
 import os
-import asyncio
-import discord
 import models
+from discord_slash import SlashCommand
 from dotenv import load_dotenv
 from discord.ext import commands
-from discord.ext.commands import has_permissions
-from datetime import datetime
+
+comandos = ['purge']
+
+
+def carregar_comandos(bot):
+    for comando in comandos:
+        try:
+            bot.load_extension(f'comandos.{comando}')
+        except Exception as e:
+            exc = f'{type(e).__name__}: {e}'
+            print(f'Erro ao carregar comando {comando}\n{exc}')
 
 
 def iniciar_bot():
-    models.criar_tabelas()
-    # carrega as variáveis de ambiente (as variáveis que não podem ser públicas, como o token do Bot)
-    load_dotenv()
+    models.criar_tabelas()  # carrega as tabelas do banco de dados
+    load_dotenv()  # carrega as variáveis de ambiente (arquivo .env)
     token = os.getenv('DISCORD_TOKEN')
     bot = commands.Bot(command_prefix='!')
+
+    slash = SlashCommand(bot, sync_commands=True, sync_on_cog_reload=True)  # Não apagar essa linha
+    carregar_comandos(bot)
 
     @bot.event
     async def on_ready():
         print(f'{bot.user} se conectou ao Discord!')
-
-    @bot.command()
-    @has_permissions(kick_members=True)
-    async def purge(ctx, qtde=1):
-        """
-        Apaga as n últimas mensagens do chat
-        :param ctx: Contexto
-        :param qtde: Quantidade de mensagens a serem apagadas. Valor padrão: 1
-        """
-
-        # apaga a mensagem do comando
-        await ctx.message.delete()
-
-        if qtde > 100:
-            qtde = 100
-        else:
-            if qtde < 1:
-                qtde = 1
-
-        async for mensagem in ctx.history(limit=qtde):
-            try:
-                await mensagem.delete()
-            finally:
-                break
-
-        horario = datetime.now().strftime('%H:%M')
-        embed = discord.Embed(title=f'Um administrador limpou {qtde} mensagens do chat', color=0x00afff)
-        embed.set_author(name=f'🧹 O chat foi limpo com sucesso!')
-        embed.set_footer(text=f'Hoje às {horario}')
-        msg = await ctx.send(embed=embed)
-        await asyncio.sleep(30)
-        await msg.delete()
-
-    @purge.error
-    async def purge_error(ctx):
-        await ctx.message.delete()
 
     bot.run(token)
 
