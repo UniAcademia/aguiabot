8875e01680971098017317558d0cc44e8ba97f10
phellipe <phellipe.duarte@icloud.com>
2021-06-04 17:02:19 -0300

    teste

---
+++ comandos/expulsar.py
@@ -0,0 +1,42 @@
+from discord.ext.commands import has_permissions, Context
+from discord.ext import commands
+from utils import utils
+
+
+class Expulsar(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command()
+    @has_permissions(kick_members=True)
+    async def expulsar(self, ctx: Context, mencao, *motivo):
+        await ctx.message.delete()
+
+        # region Obtém objeto do usuário mencionado
+        id_usuario = utils.somente_numeros(mencao)
+        if not id_usuario:
+            return
+        id_usuario = int(id_usuario)
+        usuario = await self.bot.fetch_user(id_usuario)
+        # endregion
+
+        # region Validações
+        try:
+            if usuario.permissions_in(ctx.message.channel).kick_members:
+                return
+        finally:
+            # O usuário não está no servidor
+            pass
+
+        motivo = ' '.join(motivo)
+        if usuario is None or not motivo:
+            return
+        # endregion
+
+    @expulsar.error
+    async def expulsar_error(self, ctx: Context):
+        await ctx.message.delete()
+
+
+def setup(bot):
+    bot.add_cog(Expulsar(bot))
